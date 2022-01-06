156854a04f5adec5079caced3ec68c7da4a3bdaf
Ash Bellett <ash.bellett.ab@gmail.com>
2022-01-06 12:56:48 +1100

    Add initial R scripts

---
+++ analytics/packages.R
@@ -0,0 +1,8 @@
+options(repos = list(CRAN="http://cran.rstudio.com/"))
+
+install.packages("data.table")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("rjson")
+install.packages("shiny")
+install.packages("tidyr")
